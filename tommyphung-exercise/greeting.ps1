a5bead771ab3fc6e0ce787383af947a14a04dc3b
phutommy <50922592+phutommy@users.noreply.github.com>
2022-08-30 13:52:01 -0400

    Update greeting.ps1

--- tommyphung-exercise/greeting.ps1
+++ tommyphung-exercise/greeting.ps1
@@ -1,5 +1,4 @@
 $Name = Read-Host -Prompt 'What is your full name?'
 $Date = Get-Date -Format "dddd MM/dd/yyyy"
 $Time = Get-Date -Format "HH:mm tt"
-#$deskpath = [Environment]::GetFolderPath("Desktop")
-Write-Host "Hello $Name, today is $Date and it is $Time." #| Out-File -FilePath $deskpath\greeting.txt
+Write-Host "Hello $Name, today is $Date and it is $Time."
